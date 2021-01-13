f593f05976256210602ef871fdd8ffd51f6879bd
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-01-12 21:16:36 -0800

    if-else-statemnets

---
+++ script.js
@@ -0,0 +1,12 @@
+let weather ='cloudy'
+if(weather === 'rainy'){
+    console.log('You need a rain coat.')
+} 
+
+else if(weather === 'cloudy'){
+    console.log('It might be cold , you may need a jacket')
+}
+
+else if(weather ==='sunny'){
+    console.log('Go out freeely')
+} else {'No need fro a coat'};
